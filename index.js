a24ac5f8dff0aabefef024c9d0beffe54b7453f4
Afsaneh <a.habibi1394@gmail.com>
2020-03-28 21:26:57 +0430

    testApi called from inex.js, no seccessful debug accomplisheda;

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+let testAPIRouter = require("./api/test");
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use(cors());
-app.set('port', process.env.PORT );
+//
+app.use("/",testAPIRouter);
+//
+app.set('port',process.env.PORT );
 app.listen(app.get('port'));
