886bc1e81be641b9e1916b00bf37a8174267d4e6
Afsaneh <a.habibi1394@gmail.com>
2020-03-31 19:02:53 +0430

    apies added to server

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ let map = require("./res/init.json");
 const cors = require('cors');
 let testAPIRouter = require("./api/test");
 let findPointAPIRouter = require("./api/find_point");
+let addPolygonAPIRouter = require("./api/add_polygon");
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use(cors());
 exports.map=map;
-app.use("/",testAPIRouter);
+app.use("/",addPolygonAPIRouter);
 app.use("/",findPointAPIRouter);
 app.set('port',process.env.PORT );
 app.listen(app.get('port'));
