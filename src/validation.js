1912be81a693cc8c46407979437f7fa0ce9fb962
Afsaneh <a.habibi1394@gmail.com>
2020-03-31 18:59:46 +0430

    function for geojson validation added

---
+++ src/validation.js
@@ -0,0 +1,14 @@
+export function validateGeoJson(Geojson){
+    if(!Geojson.type || Geojson.type != "Feature")
+        return false;
+    if(!Geojson.properties || !Geojson.properties.name)
+        return false;
+    if(!Geojson.geometry || !Geojson.geometry.type ||  Geojson.geometry.type!="Polygon")
+        return false;
+    if( !Geojson.geometry.coordinates || !Array.isArray(Geojson.geometry.coordinates) ||
+            !Array.isArray(Geojson.geometry.coordinates[0]))
+        return false;
+    if(Geojson.geometry.coordinates[0].length<3)
+        return false;
+    return true;
+}
