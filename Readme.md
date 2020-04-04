f0fc993187307edf03dc18a83e2271ef0115335a
a.habibi1394@gmail.com <a.habibi1394@gmail.com>
2020-04-04 21:13:49 +0430

    new validation functon used for polygon validation

--- Readme.md
+++ Readme.md
@@ -5,5 +5,6 @@ and 'winston' handels other loges
 data base file located in res/init.js
 server tested with requests in file test.http
 server deploy and test was seccessful
+validation of polygon sended to server use example in hw and does not check any extra feature to that
 for running the server use 
     npm start 
--- api/add_polygon.js
+++ api/add_polygon.js
@@ -1,10 +1,10 @@
-let validateGeoJson = require('./../src/validation');
+let val = require('./../src/validation');
 let variable =require('./../index');
 let express = require("express");
 let router = express.Router();
 const fs = require('fs');
 router.put("/gis/addpolygon",function(req,res){
-    if(!req.body || !validateGeoJson(req.body)){
+    if(!req.body || !val.validateGeoJson(req.body)){
         res.status(400).json({message:"bad request"});
     } else {
         variable.map.features.push(req.body);  
