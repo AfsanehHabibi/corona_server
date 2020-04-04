411dc0eebf9be8f6090cb89a7f116d47973d920d
Afsaneh <a.habibi1394@gmail.com>
2020-04-04 16:32:15 +0430

    validation method for Geojson became more strict, validation for point in long,lat format added

--- src/validation.js
+++ src/validation.js
@@ -8,8 +8,23 @@ function validateGeoJson(Geojson){
     if( !Geojson.geometry.coordinates || !Array.isArray(Geojson.geometry.coordinates) ||
             !Array.isArray(Geojson.geometry.coordinates[0]))
         return false;
-    if(Geojson.geometry.coordinates[0].length<3)
+    let len=Geojson.geometry.coordinates[0].length;
+    if(len<3)
+        return false;
+    if((Geojson.geometry.coordinates[0]).filter(point => isPoint(point)).length !== len)
         return false;
     return true;
 }
-module.exports=validateGeoJson;
+function isPoint(point){
+    if(!Array.isArray(point))
+        return false;
+    if(point.length!==2)
+        return false;
+    if((typeof point[0]!=="number") || (typeof point[1]!=="number"))
+        return false;
+    if(point[0]<-180 || point[0]>80 || point[1]<-90 || point[1]>90)
+        return false;
+    return true;
+}
+exports.validateGeoJson=validateGeoJson;
+exports.isPoint=isPoint;
